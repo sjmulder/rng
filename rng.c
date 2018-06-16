88700d8bc7bcfb35426d8741b92a6c0ad680dc57
Sijmen J. Mulder <ik@sjmulder.nl>
2018-06-17 01:17:51 +0200

    Remove option parsing

--- rng.c
+++ rng.c
@@ -8,47 +8,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <getopt.h>
 #include <err.h>
 
-#define PROGNAME "rng"
-#define RNG_VERSION "1.0.0"
-
-static const char usage[] =
-"Usage: rng [OPTIONS]\n\n"
-"OPTIONS:\n\n"
-" -h, --help     Show help information.\n"
-" -v, --version  Show version number.\n";
-
-int main (int argc, char **argv) {
-	int count, start, end, opt_value, long_opt_index;
+int
+main(int argc, char **argv) {
+	int count, start, end;
 	char *line;
 	size_t len;
 	ssize_t read;
 
-	long_opt_index = 0;
+	(void)argc;
+	(void)argv;
 
-	static struct option long_options[] = {
-		{ "help", no_argument, 0, 'h' },
-		{ "version", no_argument, 0, 'v' },
-	};
-
-	while ((opt_value = getopt_long(argc, argv, "hv", long_options, &long_opt_index)) != -1) {
-		switch (opt_value) {
-			case 'h':
-				fputs(usage, stderr);
-				exit(EXIT_SUCCESS);
-			case 'v':
-				fprintf(stdout, "%s\n", RNG_VERSION);
-				exit(EXIT_SUCCESS);
-			case '?':
-				fputs(usage, stderr);
-				exit(EXIT_FAILURE);
-		}
+	if (argc != 2) {
+		fputs("usage: rng from[,to] <foo >bar\n", stderr);
+		return 1;
 	}
 
-	if (argc != 2)
-		errx(1, "invalid number of arguments");
 	if (sscanf(argv[1], "%d,%d", &start, &end) != 2)
 		errx(1, "invalid range format");
 
