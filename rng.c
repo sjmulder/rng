28a632bee82601624b3351adf9b6d5289c3752e1
Sijmen J. Mulder <ik@sjmulder.nl>
2018-06-17 01:13:09 +0200

    Actually, just use sscanf

--- rng.c
+++ rng.c
@@ -20,24 +20,6 @@ static const char usage[] =
 " -h, --help     Show help information.\n"
 " -v, --version  Show version number.\n";
 
-static void
-parse_range(char *s, int *start, int *end)
-{
-	char *endptr;
-
-	*start = (int)strtol(s, &endptr, 10);
-	if (s == endptr || (*endptr && *endptr != ','))
-		errx(1, "invalid start line number");
-
-	if (*endptr == ',') {
-		s = endptr+1;
-		*end = (int)strtol(s, &endptr, 10);
-		if (s == endptr || *endptr)
-			errx(1, "invalid end line number");
-	} else
-		*end = 0;
-}
-
 int main (int argc, char **argv) {
 	int count, start, end, opt_value, long_opt_index;
 	char *line;
@@ -67,8 +49,9 @@ int main (int argc, char **argv) {
 
 	if (argc != 2)
 		errx(1, "invalid number of arguments");
+	if (sscanf(argv[1], "%d,%d", &start, &end) != 2)
+		errx(1, "invalid range format");
 
-	parse_range(argv[1], &start, &end);
 	count = 1;
 
 	while ((read = getline(&line, &len, stdin)) != -1) {
